c1569dc21f5b79310c722a01086f8e8515a01df4
xtrahuman <okpara_tochukwu@yahoo.com>
2022-08-26 11:04:18 +0100

    install dependencies, filtered image via public url, and deployed

---
+++ src/aws.ts
@@ -0,0 +1,10 @@
+import AWS = require('aws-sdk');
+import { config } from './config/config';
+
+const c = config.dev;
+
+//Configure AWS
+if(c.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+ AWS.config.credentials = credentials;
+}
---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    "dev": {
+      "aws_region": process.env.AWS_REGION, 
+      "aws_profile": process.env.AWS_PROFILE,
+    }
+  }
+  
